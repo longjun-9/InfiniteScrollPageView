71fe0af9c7f5be1c9d042f53c6292f69719a6d5f
longjun-9 <longjun9@gmail.com>
2017-01-09 11:35:59 +0800

    commit

---
+++ InfiniteScrollPageView/CustomContentView.h
@@ -0,0 +1,15 @@
+//
+//  CustomContentView.h
+//  InfiniteScrollPageView
+//
+//  Created by vipabc on 2017/1/5.
+//  Copyright © 2017年 vipabc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PageContentView.h"
+
+@interface CustomContentView : PageContentView
+@property (nonatomic, strong) UILabel *label;
+
+@end
